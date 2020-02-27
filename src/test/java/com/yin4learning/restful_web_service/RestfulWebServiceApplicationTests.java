5edf056fd1bf89421f94a0d1ec0ab8fa1f99663b
isaack <isaac4learn@gamil.com>
2020-02-26 22:43:51 -0500

    let's get started

---
+++ src/test/java/com/yin4learning/restful_web_service/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yin4learning.restful_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
