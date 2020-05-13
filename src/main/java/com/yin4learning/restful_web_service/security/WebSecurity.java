3fb1898bd640f9e0b338ecd0c97080095bf27246
unknown <isaac4learn@gmail.com>
2020-05-12 23:32:04 -0400

    update security filter

--- src/main/java/com/yin4learning/restful_web_service/security/WebSecurity.java
+++ src/main/java/com/yin4learning/restful_web_service/security/WebSecurity.java
@@ -26,7 +26,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
-			.anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+			.anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
 	}
 	
 	//AuthenticationManagerBuilder for protect password and load the encoded password
@@ -34,4 +34,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
+	
+	public AuthenticationFilter getAuthenticationFilter() throws Exception {
+	    final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+	    filter.setFilterProcessesUrl("/RestfulController/login");
+	    return filter;
+	}
 }
